e7456c9642358bb354980b956c2f2fd3075f1601
airsaint <justinxyoon@gmail.com>
2023-02-22 15:40:57 -0600

    vite files

---
+++ src/api/request.jsx
@@ -0,0 +1,8 @@
+const requests = {
+  all: "/top/anime?limit=6",
+  airing: "/top/anime?type=tv&filter=airing&limit=6",
+  upcoming: "/top/anime?type=tv&filter=upcoming&limit=6",
+  bypopularity: "/top/anime?type=tv&filter=bypopularity&limit=6",
+};
+
+export default requests;
